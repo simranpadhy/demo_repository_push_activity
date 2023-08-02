ef4392422f304d5a1e95271a1baabb67962a5eaa
simran123 <simranpadhy0@gmail.com>
2023-08-02 14:11:16 +0530

    Java file added by dev branch

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+class ReverseString{
+	public static void main(String[] args){
+
+		String name = "simran";
+		int length = name.length();
+		String reverse = "";
+		for(int i=length-1;i>=0;i++){
+			rev=rev+name.charAt(i);
+			}
+		System.out.println("Reverse of " +name+rev);
+		 
+}
+
+}
